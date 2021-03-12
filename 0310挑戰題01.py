193c70a0d6d822cdcfa8eef0fe21e9960edb867f
Marcus034 <79582792+Marcus034@users.noreply.github.com>
2021-03-12 12:07:07 +0800

    Add files via upload

---
+++ 0310挑戰題01.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+count = 1
+sum = 0
+a=1
+b=2
+
+while count < 21:
+    sum += b/a
+    a , b = b , a+b
+    count += 1
+
+print(sum)
+
+        
+    
